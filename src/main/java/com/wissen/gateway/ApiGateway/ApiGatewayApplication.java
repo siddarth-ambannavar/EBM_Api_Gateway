673709478c0e144f9f515cb30f1d0f7282f12cde
siddarth-ambannavar <siddharth.ambannavar@wissen.com>
2024-04-14 17:39:54 +0530

    added cross origin

--- src/main/java/com/wissen/gateway/ApiGateway/ApiGatewayApplication.java
+++ src/main/java/com/wissen/gateway/ApiGateway/ApiGatewayApplication.java
@@ -3,9 +3,11 @@ package com.wissen.gateway.ApiGateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@CrossOrigin({"http://localhost:4200"})
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
